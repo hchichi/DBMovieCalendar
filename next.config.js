3b0622f2fe4e66b7378de3a0352cc980d508a45b
CC <chichi@skiff.com>
2024-12-19 17:56:50 +0800

    UPDATED

--- next.config.js
+++ next.config.js
@@ -7,8 +7,29 @@ const nextConfig = {
         hostname: '**.doubanio.com',
         pathname: '/view/photo/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img1.doubanio.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img2.doubanio.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img3.doubanio.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img9.doubanio.com',
+        pathname: '/**',
+      }
     ],
   },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
